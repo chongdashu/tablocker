1917de524f9da8800af4ab9e2744668601e37a0f
Chong-U Lim <me@chongdashu.com>
2024-09-15 23:06:00 +0100

    Fix power icon from unicode to svg

--- chrome-extension/src/popup.ts
+++ chrome-extension/src/popup.ts
@@ -49,11 +49,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function updateToggleUI(isBlocking: boolean) {
     if (isBlocking) {
-      powerIcon.textContent = '⏻';
+      powerIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24">
+        <path d="M13 3h-2v10h2V3zm4.83 2.17l-1.42 1.42C17.99 7.86 19 9.81 19 12c0 3.87-3.13 7-7 7s-7-3.13-7-7c0-2.19 1.01-4.14 2.58-5.42L6.17 5.17C4.23 6.82 3 9.26 3 12c0 4.97 4.03 9 9 9s9-4.03 9-9c0-2.74-1.23-5.18-3.17-6.83z"/>
+      </svg>`;
       powerIcon.classList.remove('text-red-500');
       powerIcon.classList.add('text-green-500');
     } else {
-      powerIcon.textContent = '⏻';
+      powerIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24">
+        <path d="M13 3h-2v10h2V3zm4.83 2.17l-1.42 1.42C17.99 7.86 19 9.81 19 12c0 3.87-3.13 7-7 7s-7-3.13-7-7c0-2.19 1.01-4.14 2.58-5.42L6.17 5.17C4.23 6.82 3 9.26 3 12c0 4.97 4.03 9 9 9s9-4.03 9-9c0-2.74-1.23-5.18-3.17-6.83z"/>
+      </svg>`;
       powerIcon.classList.remove('text-green-500');
       powerIcon.classList.add('text-red-500');
     }
