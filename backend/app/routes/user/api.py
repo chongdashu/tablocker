08ac04322842b757328816b27051e9e91c660e6c
Chong-U Lim <me@chongdashu.com>
2024-09-21 23:12:21 +0100

    Add skeleton

--- backend/app/routes/user/api.py
+++ backend/app/routes/user/api.py
@@ -1,1 +1,22 @@
+from pydantic import BaseModel
 
+
+class BlockedPattern(BaseModel):
+    """
+    Represents the pattern of blocked url
+    Attributes:
+        urls (list[str]): The list of blocked urls.
+    """
+
+    pattern: str
+
+
+class SyncBlockedPatternsResponse(BaseModel):
+    """
+    Represents the pattern of blocked url
+    Attributes:
+        urls (list[str]): The list of blocked urls.
+    """
+
+    success: bool
+    blocked_patterns: list[BlockedPattern]
--- backend/app/routes/user/router.py
+++ backend/app/routes/user/router.py
@@ -1,3 +1,17 @@
+from backend.app.routes.user.api import BlockedPattern
+from backend.app.routes.user.api import SyncBlockedPatternsResponse
 from fastapi import APIRouter
 
 router = APIRouter()
+
+
+@router.get("/blocklist", response_model=list[BlockedPattern])
+async def get_blocklist() -> list[BlockedPattern]:
+    # TODO
+    return []
+
+
+@router.post("/blocklist/sync", response_model=SyncBlockedPatternsResponse)
+async def sync_blocklist() -> SyncBlockedPatternsResponse:
+    # TODO
+    return SyncBlockedPatternsResponse(success=True, blocked_patterns=[])
