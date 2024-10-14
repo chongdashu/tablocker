e67a0b4c65eaebebdee9eb83a728c20f969a1633
Chong-U Lim <me@chongdashu.com>
2024-10-14 14:18:19 +0100

    Add timestamp mixins

---
+++ backend/app/database/mixins.py
@@ -0,0 +1,11 @@
+from sqlalchemy import DateTime
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.sql import func
+
+
+class TimestampMixin:
+    created_at: Mapped[DateTime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    updated_at: Mapped[DateTime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
+    )
